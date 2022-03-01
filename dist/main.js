c6ddc7c8b8d132844354dcd05e587f56ea04a9e4
Fares <kherbanefares@gmail.com>
2022-03-01 18:52:33 +0100

    first commit

---
+++ dist/main.js
@@ -0,0 +1,61 @@
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+	anchor.addEventListener("click", function (e) {
+		e.preventDefault();
+
+		document.querySelector(this.getAttribute("href")).scrollIntoView({
+			behavior: "smooth",
+		});
+	});
+});
+
+const headerTitle = document.getElementById("header-title");
+const mobileMenu = document.querySelector("#mobile-menu");
+const btn = document.querySelector("#mobile-button");
+const content = document.getElementById("content");
+
+const mobileNavBar = document.getElementById("mobile-navbar");
+const navbar = document.querySelector("nav");
+const navbarActive = document.getElementById("navbar");
+const offsetY = 50;
+
+window.addEventListener("scroll", (e) => {
+	console.log(window.pageYOffset);
+	if (window.pageYOffset > offsetY) {
+		
+	} else if (window.pageYOffset < offsetY) {
+		
+	}
+});
+
+let menuToggle = false;
+
+mobileMenu.style.right = "-600px";
+
+btn.addEventListener("click", (e) => {
+	if (menuToggle == false) {
+		mobileMenu.style.right = "0px";
+		headerTitle.style.marginLeft = "-600px";
+		btn.style.marginRight = "47.5%";
+		mobileNavBar.classList.remove("bg-opacity-30");
+		mobileNavBar.classList.remove("bg-black");
+		mobileNavBar.classList.remove("backdrop-blur-md");
+		menuToggle = true;
+	} else if (menuToggle == true) {
+		mobileMenu.style.right = "-600px";
+		headerTitle.style.marginLeft = "0px";
+		mobileNavBar.classList.add("bg-opacity-30");
+		mobileNavBar.classList.add("bg-black");
+		mobileNavBar.classList.add("backdrop-blur-md");
+		btn.style.marginRight = "0px";
+		menuToggle = false;
+	}
+});
+content.addEventListener("click", () => {
+	mobileMenu.style.right = "-600px";
+	headerTitle.style.marginLeft = "0px";
+	btn.style.marginRight = "0px";
+	mobileNavBar.classList.add("bg-opacity-30");
+	mobileNavBar.classList.add("bg-black");
+	mobileNavBar.classList.add("backdrop-blur-md");
+	menuToggle = false;
+});
---
+++ tailwind.config.js
@@ -0,0 +1,67 @@
+module.exports = {
+	content: ["./dist/*.html"],
+	theme: {
+		fontSize: {
+			xs: ".75rem",
+			sm: ".875rem",
+			tiny: ".875rem",
+			base: "1rem",
+			lg: "1.125rem",
+			xl: "1.25rem",
+			"2xl": "1.5rem",
+			"3xl": "1.875rem",
+			"4xl": "2.25rem",
+			"5xl": "3rem",
+			"6xl": "4rem",
+			"7xl": "5rem",
+			"1vw": "10vw",
+			"2vw": "20vw",
+			"3vw": "30vw",
+		},
+		screens: {
+			sm: "640px",
+			// => @media (min-width: 640px) { ... }
+
+			md: "768px",
+			// => @media (min-width: 768px) { ... }
+
+			lg: "1024px",
+			// => @media (min-width: 1024px) { ... }
+
+			xl: "1280px",
+			// => @media (min-width: 1280px) { ... }
+
+			"2xl": "1536px",
+			// => @media (min-width: 1536px) { ... }
+		},
+		extend: {
+			backgroundImage: {
+				header: "url('img/background.jpg')",
+				profile: "url('img/about_pic.jfif')",
+			},
+			fontFamily: {
+				custom: ["Bebas Neue", "mono"],
+			},
+			keyframes: {
+				typewriter: {
+					from: { width: "0" },
+					to: { width: "100%" },
+				},
+				blink: {
+					from: { borderColor: "transparent" },
+					to: { borderColor: "white" },
+				},
+				nav: {
+					"0%": { opacity: "0", marginTop: "-200px" },
+					"100%": { opacity: "1", marginTop: "0px" },
+				},
+			},
+			animation: {
+				typewriter:
+					"typewriter 3s steps(120, end) forwards, blink .5s infinite",
+				nav: "nav 1s forwards",
+			},
+		},
+	},
+	plugins: [],
+};
